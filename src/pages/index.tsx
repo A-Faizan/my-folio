26765e9fcf15e7e01d055d1b7544725490257d26
faizan <faizan@eforte.net>
2023-02-21 18:47:30 +0500

    Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/Navbar";
+import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Navbar />;
+  return <div></div>;
 }
